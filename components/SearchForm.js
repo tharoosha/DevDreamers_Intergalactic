0621a742e12bd7f4898b5493e77151e7f381d146
tharoosha <tharooshavihidun@gmail.com>
2023-08-18 15:34:17 +0530

    Finalizing the Search Form

--- components/SearchForm.js
+++ components/SearchForm.js
@@ -2,8 +2,18 @@ import { View, Text, StyleSheet,TextInput, TouchableOpacity, SafeAreaView } from
 import React, { useState } from 'react'
 import { Color, Border, FontFamily, FontSize } from "../constants/GlobalStyles";
 import {Button, Input, DateTime} from '../components'
+// import moment from 'moment';
+
+
+// import DateTimePicker from '@react-native-community/datetimepicker';
+import DateTimePicker from "react-native-modal-datetime-picker";
+
+import { Feather } from '@expo/vector-icons';
+// import { useRouter } from 'expo-router';
 
 const SearchForm = () => {
+  // const router = useRouter();
+
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [date, setDate] = useState('');
@@ -13,7 +23,40 @@ const SearchForm = () => {
     console.log('Name:', name);
     console.log('Email:', email);
     console.log('Date:', date);
-    console.log('Mode',mode)  };
+    console.log('Mode',mode)  
+  };
+
+  const [selectedDate, setSelectedDate] = useState('');
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = (date) => {
+
+    const CDateString = date.toString();
+    const months = [
+      "01", "02", "03", "04", "05", "06",
+      "07", "08", "09", "10", "11", "12"
+    ];
+
+    const year = date.getFullYear();
+    const month = months[date.getMonth()];
+    const day = (date.getDate() < 10) ? `0${date.getDate()}` : date.getDate();
+ 
+    const formattedDate = `${day}/${month}/${year}`;
+    console.log('Date:', formattedDate);
+
+    if (date) {
+      setDate(formattedDate);
+    }
+    hideDatePicker();
+  };
 
   return (
     // <SafeAreaView style={styles.container}>
@@ -34,7 +77,8 @@ const SearchForm = () => {
           label={"To"}
         />
         <View style={styles.row}>
-          <View style={styles.inputWrap}>
+          <View style={styles.DateTimeContainer}>
+            <Text style={styles.input_text}>Date</Text>
             {/* <Input
               placeholder={"12/08/2157"}
               value={date}
@@ -42,7 +86,39 @@ const SearchForm = () => {
               width={145}
               label={"Date"}
             /> */}
-            <DateTime />
+            {/* <DateTime /> */}
+            <View style={styles.searchWrapper}>
+              <TextInput
+                style={styles.searchInput}
+                value={date}
+                onChangeText={(text) => setDate(text)}
+                placeholder={'DD/MM/YYYY'}
+                placeholderTextColor={Color.white}
+                
+              />
+              <TouchableOpacity style={styles.searchBtn} onPress={showDatePicker}>
+                {/* <Image
+                  source={icons.search}
+                  resizeMode='contain'
+                  style={styles.searchBtnImage}
+                /> */}
+                <Feather name="calendar" size={24} color={Color.white} style={styles.calendarIcon} />
+              </TouchableOpacity> 
+            </View>
+
+            
+            
+            <DateTimePicker
+                isVisible = {isDatePickerVisible}
+                mode="date"
+                // is24Hour={true}
+                // display="default"
+                // onChange={(event, date) => handleConfirm(date)}
+                onConfirm={handleConfirm}
+                onCancel={hideDatePicker}
+            />
+            
+              
           </View>
           <View style={styles.inputWrap}>
             <Input
@@ -100,6 +176,46 @@ const styles = StyleSheet.create({
     alignContent: 'center',
     marginLeft: 5
   },
+  DateTimeContainer:{
+    flex: 1,
+    alignContent: 'center'
+  },
+  input_text: {
+    fontSize: 12,
+    lineHeight: 18,
+    fontWeight: "700",
+    fontFamily: "Poppins-Bold",
+    color: "#fff",
+    textAlign: "left",
+    width: 80,
+    height: 13,
+    margin: 7,
+    paddingLeft: 10
+  },
+  searchWrapper:{
+    flexDirection: "row",
+    justifyContent: 'space-between', 
+    borderRadius: 44,
+    backgroundColor: "#091522",
+    borderStyle: "solid",
+    borderColor: "#3a3a42",
+    borderWidth: 1,
+    height: 44,
+    fontFamily: FontSize.poppinsRegular,
+    color: "#fff",
+    paddingLeft: 17,
+    marginBottom: 5,
+    alignItems: "center", 
+  },
+  searchInput:{
+    color: Color.white,
+    width: 88
+  },
+  searchBtn:{
+    // marginTop:8,
+    // marginLeft: 1.2
+    width: 40
+  }
 });
 
 export default SearchForm;
