544f4ca189fe58b80582ed780fd3c811a055bb46
muaadh99 <muaadhmnm.20@uom.lk>
2023-08-20 19:35:42 +0530

    Horizontal Line Comp Added

---
+++ components/HorozontaLine.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const HorizontalLine = ({ color, height , opacity ,topmargin , bottommargin }) => {
+  const lineStyle = {
+    borderBottomColor: color || 'black',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    height: height || 1,
+    opacity: opacity || 1,
+    marginBottom: bottommargin || 0,
+    marginTop: bottommargin || 0,
+  };
+
+  return <View style={[lineStyle, styles.horizontalLine]} />;
+};
+
+const styles = StyleSheet.create({
+  horizontalLine: {
+    alignSelf: 'stretch',
+  },
+});
+
+export default HorizontalLine;
